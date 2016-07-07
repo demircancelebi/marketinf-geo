cff9347c6085fdde7a5dd7676977d3ef46621ba4
Baris Palaska <barispalaska@gmail.com>
2016-07-07 19:26:47 +0300

    add cors

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
